dd0c8f40e05f76d106c6afb1d14fac354198db85
bakebrlkk <210103194@stu.sdu.edu.kz>
2023-10-26 14:04:26 +0600

    profile_ui

---
+++ OrangeHACKATHON/OrangeHACKATHON/ProfileView.swift
@@ -0,0 +1,88 @@
+//
+//  ProfileView.swift
+//  Culinary Oasis
+//
+//  Created by bakebrlk on 26.10.2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        
+        VStack{
+                
+            title
+            
+            profileImage
+            
+            description(text: "Bekzat Birlik")
+                .font(.title2)
+                
+                    
+            posts
+            
+            Spacer()
+        }
+    
+    }
+    
+    private var posts: some View {
+        VStack{
+            post(title: titleInfo(text: "Телефон"), description: description(text: "+7 700 241 9495"))
+            post(title: titleInfo(text: "E-mail"), description: description(text: "bekzatbirlik@gmail.com"))
+            post(title: titleInfo(text: "карта"), description:  Image("visa").padding())
+            post(title: titleInfo(text: "адрес"), description: description(text: "Алматы, Жунисова 4/6 1 под, 36кв"))
+            
+        }
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(16)
+        .padding()
+    }
+    
+    private func post(title: some View, description: some View) -> some View{
+        HStack{
+            title
+               
+            
+            Spacer()
+            description
+        }
+    }
+    
+    private func titleInfo(text: String) -> some View{
+        Text(text)
+            .foregroundStyle(Color.gray)
+            .padding()
+    }
+    
+    private func description(text: String) -> some View{
+        Text(text)
+            .bold()
+            .fontWidth(.compressed)
+            .padding()
+    }
+    
+    private var title: some View = {
+        Text("Profile")
+            .bold()
+            .fontWidth(.condensed)
+            .padding()
+    }()
+    
+    private var profileImage: some View = {
+        
+        Image(systemName: "person.crop.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .padding()
+        
+    }()
+    
+}
+
+
+#Preview {
+    ProfileView()
+}
