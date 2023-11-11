ccab3e682851bec4191aff45b6f49718779d5eb1
bakebrlkk <210103194@stu.sdu.edu.kz>
2023-11-11 12:43:20 +0600

    bakebrlk

---
+++ OrangeHACKATHON/OrangeHACKATHON/OnBordingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBordingView.swift
+//  Culinary Oasis
+//
+//  Created by bakebrlk on 02.11.2023.
+//
+
+import SwiftUI
+
+struct OnBordingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBordingView()
+}
